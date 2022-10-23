0af4a3352bcbc3bd8a072558d3a143ed83a09e90
artem.karimov <artem.karimov@ecosoft.com>
2022-10-23 14:55:28 +0300

    Finish task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+const gallery = document.querySelector('.gallery');
+
+const displayImages = () => {
+  const items = [];
+
+  for (const galleryItem of galleryItems) {
+    const item = `
+      <a class="gallery__item" href="${galleryItem.original}">
+        <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+      </a>
+    `;
+    items.push(item);
+  }
+
+  gallery.insertAdjacentHTML('afterbegin', items.join('\n'));
+};
+
+displayImages();
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
