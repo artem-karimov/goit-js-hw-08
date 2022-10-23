a1e0904558fd7729cec9aa20892109532a5ebc78
artem.karimov <artem.karimov@ecosoft.com>
2022-10-23 16:09:44 +0300

    Finish task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const initializePlayer = () => {
+  const vimeoPlayer = document.getElementById('vimeo-player');
+
+  const player = new Player(vimeoPlayer);
+
+  const playerTimeKey = 'videoplayer-current-time';
+
+  const storedPlayerTime = localStorage.getItem(playerTimeKey);
+
+  if (storedPlayerTime && +storedPlayerTime > 0) {
+    player.setCurrentTime(+storedPlayerTime);
+  }
+
+  const handleTimeUpdate = data => {
+    localStorage.setItem(playerTimeKey, data.seconds);
+  };
+
+  const handleTimeUpdateThrottled = throttle(handleTimeUpdate, 1000);
+
+  player.on('timeupdate', handleTimeUpdateThrottled);
+};
+
+initializePlayer();
